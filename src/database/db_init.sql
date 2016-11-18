198c09d5c4082743730f3ae8bdb3bba4e7ff7079
Mohammed Alanazi <ma650405@sju.edu>
2016-11-18 16:41:56 -0500

    Added some tables to db_init.sql

--- src/database/db_init.sql
+++ src/database/db_init.sql
@@ -13,3 +13,74 @@
 
 -- This SQL script is compatible with MySQL only.
 -- This SQL is for database initilization for the deplyment.
+
+drop table if exists in_service;
+drop table if exists customer;
+drop table if exists vehicle;
+drop table if exists employee;
+drop table if exists garage;
+
+create table in_service (
+	in_service_id integer primary key auto_increment,
+	customer_id integer references customer(customer_id),
+	vehicle_id integer references vehicle(vehicle_id),
+	est_completion date,
+	mechanic integer references employee(employee_id),
+	garage_id integer references garage(garage_id),
+	service text
+);
+
+create table customer (
+	customer_id integer primary key auto_increment,
+	name text,
+	phone text,
+	zipcode text
+);
+
+create table vehicle (
+	vehicle_id integer primary key auto_increment,
+	owner integer references customer(customer_id),
+	year text,
+	make text,
+	model text,
+	trim text
+);
+
+create table employee (
+	employee_id integer primary key auto_increment,
+	name text,
+	phone text,
+	position enum('manager', 'mechanic')
+);
+
+create table garage (
+	garage_id integer primary key auto_increment,
+	name text,
+	zipcode text
+);
+
+insert into customer (customer_id, name, phone, zipcode) values
+(1, 'Greg Lewis', 1234567890, 19104),
+(2, 'Sophie Linn', 1245987349, 45873),
+(3, 'Juan Alvarez', 4523487190, 32132);
+
+insert into vehicle (vehicle_id, owner, year, make, model, trim) values
+(1, 1, 2016, 'Toyota', 'Camry', null),
+(2, 2, 2010, 'Chevy', 'Cruze', null),
+(3, 3, 2014, 'Honda', 'Civic', null);
+
+insert into employee (employee_id, name, phone, position) values
+(1, 'Tod',  4574892381, 'mechanic'),
+(2, 'Greg', 5487359012, 'mechanic'),
+(3, 'Bob',  2349129384, 'mechanic');
+
+insert into garage (garage_id, name, zipcode) values
+(1, 'Garage 1', 32132),
+(2, 'Garage 2', 19104),
+(3, 'Garage 3', 45873);
+
+insert into in_service (customer_id, vehicle_id, est_completion, mechanic, garage_id, service) values
+(1, 1, '2016-11-2', 1, 2, 'Oil change'),
+(2, 2, '2016-11-10', 2, 3, 'Brake repair'),
+(3, 3, '2016-11-12', 3, 1, 'Inspection');
+
