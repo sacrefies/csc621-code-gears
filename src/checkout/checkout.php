3574eb47605c067184f9119860bb3f0c6180694f
Sean_Monahan <sm600817@sju.edu>
2016-12-01 14:21:11 -0500

    Added pending and payed tabs to checkout

--- src/checkout/checkout.php
+++ src/checkout/checkout.php
@@ -40,6 +40,8 @@ include __DIR__ . '/../header.php';
 <!-- main content starts here -->
 <body>
 	<script type="text/javascript">
+		//document.getElementById("defaultOpen").click();
+
 		function popUp(id){
 			var valid = false;
 			while(valid === false)
@@ -74,44 +76,114 @@ include __DIR__ . '/../header.php';
 		function isNumeric(n) {
   			return !isNaN(parseFloat(n)) && isFinite(n);
 		}
+
+		/*function openTab(evt, tab) {
+    		// Declare all variables
+    		var i, tabcontent, tablinks;
+
+    		// Get all elements with class="tabcontent" and hide them
+    		tabcontent = document.getElementsByClassName("tabcontent");
+    		for (i = 0; i < tabcontent.length; i++) {
+        		tabcontent[i].style.display = "none";
+    		}
+
+    		// Get all elements with class="tablinks" and remove the class "active"
+    		tablinks = document.getElementsByClassName("tablinks");
+    		for (i = 0; i < tablinks.length; i++) {
+        		tablinks[i].className = tablinks[i].className.replace("active", "");
+    		}
+
+    		// Show the current tab, and add an "active" class to the link that opened the tab
+    		document.getElementById(tab).style.display = "block";
+    		evt.currentTarget.className += "active";
+		}*/
 	</script>
-	<h1 class="page-header">Pending Invoices</h1>
-<?php
+	<h1 class="page-header">Invoices</h1>
+	<div role="tabpanel">
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation" class="active"><a href="#pendingTab" role="tab" 
+				data-toggle="tab">Pending</a></li>
+        	<li role="presentation"><a href="#payedTab" role="tab" 
+        		data-toggle="tab">Payed</a></li>
+        </ul>
+        <div class="tab-content">
+	        <div id="pendingTab" role="tabpanel" class="tab-pane active">
+				<?php
 
-/*$invoice = invoice::createNew();
+				/*$invoice = invoice::createNew();
 
-$invoice->apptId = 1;
-$invoice->amtDue = 20.00;
-$invoice->calcAmtDue();*/
+				$invoice->apptId = 1;
+				$invoice->amtDue = 20.00;
+				$invoice->calcAmtDue();*/
 
-$invoices = CheckoutController::getAllPendingInvoices();
+				$invoices = CheckoutController::getAllPendingInvoices();
 
-echo "<table class='table'>";
-echo "<tr>";
-echo "<th>Invoice ID</th>";
-echo "<th>Appointment ID</th>";
-echo "<th>Time Created</th>";
-echo "<th>Last Updated</th>";
-echo "<th>Amount Due</th>";
-echo "<th>Amount Payed</th>";
-echo "<th>Discount</th>";
-echo "</tr>";
-foreach($invoices as $invoice) {
-	$id = $invoice->invoiceId;
-	echo "<tr>";
-	echo "<td>" . $id . "</td>";
-	echo "<td>" . $invoice->apptId . "</td>";
-	echo "<td>" . $invoice->createTime . "</td>";
-	echo "<td>" . $invoice->updateTime . "</td>";
-	echo "<td>" . $invoice->amtDue . "</td>";
-	echo "<td>" . $invoice->amtPayed . "</td>";
-	echo "<td>" . $invoice->discRate . "</td>";
-	echo "<td><button class='button' onclick=popUp($id)>Update</button></td>";
-	echo "</tr>";
-}
-	
-echo "</table>";
-?>
-<p id="output"></p>
+				echo "<table class='table'>";
+				echo "<tr>";
+				echo "<th>Invoice ID</th>";
+				echo "<th>Appointment ID</th>";
+				echo "<th>Time Created</th>";
+				echo "<th>Last Updated</th>";
+				echo "<th>Amount Due</th>";
+				echo "<th>Amount Payed</th>";
+				echo "<th>Discount</th>";
+				echo "</tr>";
+				foreach($invoices as $invoice) {
+					$id = $invoice->invoiceId;
+					echo "<tr>";
+					echo "<td>" . $id . "</td>";
+					echo "<td>" . $invoice->apptId . "</td>";
+					echo "<td>" . $invoice->createTime . "</td>";
+					echo "<td>" . $invoice->updateTime . "</td>";
+					echo "<td>" . $invoice->amtDue . "</td>";
+					echo "<td>" . $invoice->amtPayed . "</td>";
+					echo "<td>" . $invoice->discRate . "</td>";
+					echo "<td><button class='button' onclick=popUp($id)>Update</button></td>";
+					echo "</tr>";
+				}
+					
+				echo "</table>";
+				?>
+			</div>
+			<div id="payedTab" role="tabpanel" class="tab-pane">
+				<?php
+
+				/*$invoice = invoice::createNew();
+
+				$invoice->apptId = 1;
+				$invoice->amtDue = 20.00;
+				$invoice->calcAmtDue();*/
+
+				$invoices = CheckoutController::getAllPayedInvoices();
+
+				echo "<table class='table'>";
+				echo "<tr>";
+				echo "<th>Invoice ID</th>";
+				echo "<th>Appointment ID</th>";
+				echo "<th>Time Created</th>";
+				echo "<th>Last Updated</th>";
+				echo "<th>Amount Due</th>";
+				echo "<th>Amount Payed</th>";
+				echo "<th>Discount</th>";
+				echo "</tr>";
+				foreach($invoices as $invoice) {
+					$id = $invoice->invoiceId;
+					echo "<tr>";
+					echo "<td>" . $id . "</td>";
+					echo "<td>" . $invoice->apptId . "</td>";
+					echo "<td>" . $invoice->createTime . "</td>";
+					echo "<td>" . $invoice->updateTime . "</td>";
+					echo "<td>" . $invoice->amtDue . "</td>";
+					echo "<td>" . $invoice->amtPayed . "</td>";
+					echo "<td>" . $invoice->discRate . "</td>";
+					echo "</tr>";
+				}
+					
+				echo "</table>";
+				?>
+			</div>
+		</div>
+	</div>
+	<p id="output"></p>
 </body>
 <?php include __DIR__ . '/../footer.php'; ?>
