7881a6c1d6f93f747e24f12c35fa211b33bce46a
jm652564 <jasonqiaomeng@outlook.com>
2016-12-03 00:30:24 -0500

    fix time format for mysql

--- src/appointments/Appointment.php
+++ src/appointments/Appointment.php
@@ -105,9 +105,9 @@ class Appointment extends StatefulEntity {
     public function update() : int {
         // ['subject', 'update_time', 'create_time', 'description', 'event_time', 'start_time', 'end_time',
         // 'customer_id', 'state'];
-        $values = [$this->subject, $this->updateTime->format(DATE_ATOM), $this->createTime->format(DATE_ATOM),
-            $this->desc, $this->eventTime->format(DATE_ATOM), $this->startTime->format(DATE_ATOM),
-            $this->endTime->format(DATE_ATOM), $this->customer->customerId, $this->state];
+        $values = [$this->subject, $this->updateTime->format(DATE_ISO8601), $this->createTime->format(DATE_ISO8601),
+            $this->desc, $this->eventTime->format(DATE_ISO8601), $this->startTime->format(DATE_ISO8601),
+            $this->endTime->format(DATE_ISO8601), $this->customer->customerId, $this->state];
         if ($this->appId === -1) {
             return $this->insert($values);
         } else {
