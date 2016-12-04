2ea99200946f515a67eac48e3980961830cf0ad1
Sean_Monahan <sm600817@sju.edu>
2016-12-04 14:43:44 -0500

    Fix namespace

--- src/services/JobsController.php
+++ src/services/JobsController.php
@@ -16,7 +16,7 @@
  */
 
 declare(strict_types = 1);
-namespace gears\checkout;
+namespace gears\services;
 
 require_once __DIR__ . '/../Controller.php';
 require_once __DIR__ . '/../conf/Settings.php';
