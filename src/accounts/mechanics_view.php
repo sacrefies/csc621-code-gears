226b1b994cd1f44772fe6569b2cf3f0dab7396c2
jm652564 <jasonqiaomeng@outlook.com>
2016-12-07 00:00:42 -0500

    removed assignJob button

--- src/accounts/mechanics_view.php
+++ src/accounts/mechanics_view.php
@@ -82,35 +82,10 @@ $emps = AccountController::getAllEmployees();
                                 <h5><span class="label label-default">BUSY</span></h5>
                             <?php } ?>
                         </td>
-                        <td>
-                            <?php if ($emp->getState() === State::AVAILABLE) { ?>
-                                <button type="button" name="btnPickJob" class="btn btn-info btn-sm" data-toggle="modal"
-                                        data-target="#jobSelect">Assign Job
-                                </button>
-                            <?php } ?>
-                        </td>
                     </tr>
                 <?php } ?>
                 </tbody>
             </table>
-            <!-- Modal -->
-            <div id="jobSelect" class="modal fade" role="dialog">
-                <div class="modal-dialog">
-                    <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">Select One Job</h4>
-                        </div>
-                        <div class="modal-body">
-                            <p>Job List Should Be Displayed Here</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </form>
         <?php } else { ?>
             <div class="alert alert-warning alert-dismissible">
